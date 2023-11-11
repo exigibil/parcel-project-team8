b8b079df73a130a7d653bdbf7400cd49cf97e38e
cristinamacicasan <139801348+cristinamacicasan@users.noreply.github.com>
2023-11-11 08:31:03 +0000

    Deploying to gh-pages from @ exigibil/parcel-project-team8@26de214865509abb1249042595c674586c721f68 🚀

---
+++ index.61dde61a.js
@@ -0,0 +1,2 @@
+let e=0;const t=document.querySelectorAll(".image"),n=document.getElementById("imageCount");function o(){n.textContent=`Imagine ${e+1} din ${t.length}`}o();
+//# sourceMappingURL=index.61dde61a.js.map
---
+++ index.a9fc2a5c.js
@@ -0,0 +1,2 @@
+!function(){var n=0,t=document.querySelectorAll(".image"),e=document.getElementById("imageCount");function c(){e.textContent="Imagine ".concat(n+1," din ").concat(t.length)}c()}();
+//# sourceMappingURL=index.a9fc2a5c.js.map
